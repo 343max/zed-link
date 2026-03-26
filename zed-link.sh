1ea8412d832bf7020922ae90df881302d85628c0
Max von Webel <max@343max.de>
2026-03-26 14:51:42 +0100

    generating clickable OSC 8 link

--- zed-link.sh
+++ zed-link.sh
@@ -12,5 +12,8 @@ fi
 URL="zed://ssh/$HOSTNAME$WORKSPACE"
 ENCODED="$(echo -n $URL | base64 -w 0)"
 
+# WezTerm: Set user var for Lua handling
 printf "\033]1337;SetUserVar=%s=%s\007" open_url $ENCODED
-echo $URL
+
+# OSC 8: Hyperlink for clickable URLs in most modern terminals
+printf "\033]8;;%s\033\\%s\033]8;;\033\n" "$URL" "$URL"
